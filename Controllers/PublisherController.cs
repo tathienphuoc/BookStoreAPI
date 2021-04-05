b40bebd6f269cc9861c578fef3f1888849679c1f
mrjun27 <minh.hua.3720@gmail.com>
2021-04-05 15:32:59 +0700

    Add publisher

---
+++ Controllers/PublisherController.cs
@@ -0,0 +1,93 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStoreAPI.Models;
+using BookStoreAPI.Service;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using System.Collections.Generic;
+using System;
+namespace Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublisherController : ControllerBase
+    {
+        private readonly PublisherService service;
+
+        public PublisherController(PublisherService service)
+        {
+            this.service = service;
+        }
+
+        public IEnumerable<Publisher> Get()
+        {
+            return service.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Publisher> GetPublisher(int id)
+        {
+            var Publisher = service.GetDetail(id);
+
+            if (Publisher == null)
+            {
+                return NotFound();
+            }
+
+            return Publisher;
+        }
+
+
+        [HttpPost]
+        public ActionResult<Publisher> Create(PublisherCreateDto PublisherCreateDto)
+        {
+            try
+            {
+                return service.Create(PublisherCreateDto);
+            }
+            catch (Exception error)
+            {
+                return Conflict(error.Message);
+            }
+        }
+
+
+        [HttpPut]
+        public ActionResult<Publisher> Update(PublisherUpdateDto PublisherUpdateDto)
+        {
+            try
+            {
+                return service.Update(PublisherUpdateDto);
+            }
+            catch (ArgumentException error)
+            {
+                return NotFound(error.Message);
+            }
+            catch (Exception error)
+            {
+                return Conflict(error.Message);
+            }
+        }
+
+
+        [HttpDelete("{id}")]
+        public ActionResult<Publisher> DeletePublisher(int id)
+        {
+            try
+            {
+                return service.Delete(id);
+            }
+            catch (Exception error)
+            {
+                return Conflict(error.Message);
+            }
+        }
+
+
+
+
+
+    }
+}
